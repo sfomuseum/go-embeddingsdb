f62bd3cbda26771cedd83e1485231b92c81f5197
thisisaaronland <thisisaaronland@localhost>
2026-03-01 11:13:57 -0800

    tests for DeserializeFloats

--- database/sqlite_vec_test.go
+++ database/sqlite_vec_test.go
@@ -1,9 +1,33 @@
-package data
+package database
 
 import (
+	"slices"
 	"testing"
+
+	sqlite_vec "github.com/asg017/sqlite-vec-go-bindings/cgo"
 )
 
 func TestDeserializeFloat32(t *testing.T) {
-	t.Skip()
+
+	f32 := []float32{
+		0.0,
+		0.1,
+		0.002,
+	}
+
+	s32, err := sqlite_vec.SerializeFloat32(f32)
+
+	if err != nil {
+		t.Fatalf("Failed to serialize floats, %v", err)
+	}
+
+	new_f32, err := DeserializeFloat32(s32)
+
+	if err != nil {
+		t.Fatalf("Failed to deserialize floats, %v", err)
+	}
+
+	if !slices.Equal(f32, new_f32) {
+		t.Fatalf("Deserialized floats to do match input")
+	}
 }
