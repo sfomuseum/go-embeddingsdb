0c021601c12a16a3ca65bb0d058e031c923f8f46
thisisaaronland <thisisaaronland@localhost>
2026-03-26 18:32:55 -0700

    snapshot: block out upload JS

--- www/static/javascript/upload.init.js
+++ www/static/javascript/upload.init.js
@@ -7,8 +7,33 @@ window.addEventListener('load', function(e){
 
     submit.onclick = function(e){
 
-	var el = el.target;
+	const url = new URL("/upload", location);
 	
+	const form = e.target;
+	const data = new FormData(form);
+
+	const xhr = new XMLHttpRequest();
+	xhr.open('POST', u.toString());
+
+	xhr.upload.addEventListener('progress', function (ev) {
+	    
+	    if (ev.lengthComputable) {
+		const percent = Math.round((ev.loaded / ev.total) * 100);
+		// progressBar.value = percent;
+		console.log(`Upload progress: ${percent}%`);
+	    }
+	});
+
+	xhr.addEventListener('load', function () {
+	    progressBar.style.display = 'none';
+	    if (xhr.status >= 200 && xhr.status < 300) {
+		console.log('Upload succeeded', xhr.responseText);
+	    } else {
+		console.error('Upload failed', xhr.status, xhr.statusText);
+	    }
+	});
+
+	xhr.send(form);
 	console.log("Upload");
 	return false;
     };
