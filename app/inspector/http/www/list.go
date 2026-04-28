51c63cfea1e247af74ff934bda9f9cbe6bf58e3a
thisisaaronland <thisisaaronland@localhost>
2026-04-28 16:09:25 -0700

    update inspector list handler to account for cursors

--- app/inspector/http/www/list.go
+++ app/inspector/http/www/list.go
@@ -1,6 +1,7 @@
 package www
 
 import (
+	"context"
 	"fmt"
 	"html/template"
 	"net/http"
@@ -9,9 +10,11 @@ import (
 	"github.com/aaronland/go-http/v4/slog"
 	"github.com/aaronland/go-pagination"
 	"github.com/aaronland/go-pagination/countable"
+	"github.com/aaronland/go-pagination/cursor"
 	"github.com/sfomuseum/go-embeddingsdb"
 	inspector_http "github.com/sfomuseum/go-embeddingsdb/app/inspector/http"
 	"github.com/sfomuseum/go-embeddingsdb/client"
+	"github.com/sfomuseum/go-embeddingsdb/database"
 	"github.com/sfomuseum/go-embeddingsdb/options"
 )
 
@@ -41,6 +44,20 @@ func ListHandler(list_opts *ListHandlerOptions) (http.Handler, error) {
 		return nil, fmt.Errorf("Failed to load 'list' template")
 	}
 
+	ctx := context.Background()
+	pg_type, err := list_opts.Client.PaginationType(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to determine pagination type, %w", err)
+	}
+
+	switch pg_type {
+	case database.CountablePaginationType, database.CursorPaginationType:
+		// ok
+	default:
+		return nil, fmt.Errorf("Unsupported pagination type, %T", pg_type)
+	}
+
 	fn := func(rsp http.ResponseWriter, req *http.Request) {
 
 		ctx := req.Context()
@@ -62,27 +79,67 @@ func ListHandler(list_opts *ListHandlerOptions) (http.Handler, error) {
 			return
 		}
 
-		pg_opts, err := countable.NewCountableOptions()
+		var pg_opts pagination.Options
 
-		if err != nil {
-			logger.Error("Failed to create pagination options", "error", err)
+		switch pg_type {
+		case database.CountablePaginationType:
+
+			countable_opts, err := countable.NewCountableOptions()
+
+			if err != nil {
+				logger.Error("Failed to create pagination options", "error", err)
+				http.Error(rsp, "Internal server error", http.StatusInternalServerError)
+				return
+			}
+
+			countable_opts.PerPage(int64(15))
+			countable_opts.Pointer(int64(1))
+
+			page, err := sanitize.GetInt64(req, "page")
+
+			if err != nil {
+				logger.Error("Failed to derive page query parameter", "error", err)
+				http.Error(rsp, "Internal server error", http.StatusInternalServerError)
+				return
+			}
+
+			if page != 0 {
+				countable_opts.Pointer(page)
+			}
+
+			pg_opts = countable_opts
+
+		case database.CursorPaginationType:
+
+			cursor_opts, err := cursor.NewCursorOptions()
+
+			if err != nil {
+				logger.Error("Failed to create pagination options", "error", err)
+				http.Error(rsp, "Internal server error", http.StatusInternalServerError)
+				return
+			}
+
+			cursor_opts.PerPage(int64(15))
+			cursor_opts.Pointer(int64(1))
+
+			page, err := sanitize.GetString(req, "page")
+
+			if err != nil {
+				logger.Error("Failed to derive page query parameter", "error", err)
+				http.Error(rsp, "Internal server error", http.StatusInternalServerError)
+				return
+			}
+
+			if page != "" {
+				cursor_opts.Pointer(page)
+			}
+
+			pg_opts = cursor_opts
+
+		default:
+			logger.Error("Unsupported pagination type", "type", pg_type)
 			http.Error(rsp, "Internal server error", http.StatusInternalServerError)
 			return
-		}
-
-		pg_opts.PerPage(int64(15))
-		pg_opts.Pointer(int64(1))
-
-		page, err := sanitize.GetInt64(req, "page")
-
-		if err != nil {
-			logger.Error("Failed to derive page query parameter", "error", err)
-			http.Error(rsp, "Internal server error", http.StatusInternalServerError)
-			return
-		}
-
-		if page != 0 {
-			pg_opts.Pointer(page)
 		}
 
 		opts := make([]options.Option, 0)
